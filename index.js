38358dc240464c20da864f39111d41a21e156cd3
Christopher <chris.brower.96@gmail.com>
2023-01-30 17:02:07 -0700

    working lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let num3 = 6;
+let num4 = 10;
+let num5 = 20;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100);
+
+let mod = num4 % num3;
+
+let max = Math.max(num1, num5, num3, num4);
